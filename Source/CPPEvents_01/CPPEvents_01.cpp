39dab766599941d16aa24bee7a192a4f2a55658b
md_cliff <katax.emperor@gmail.com>
2022-12-28 23:45:08 +1100

    ver 1.0
    upload to github

---
+++ Source/CPPEvents_01/CPPEvents_01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CPPEvents_01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPPEvents_01, "CPPEvents_01" );
---
+++ Source/CPPEvents_01/Private/Item_01.cpp
@@ -0,0 +1,59 @@
+#include "Item_01.h"
+
+AItem_01::AItem_01(){
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AItem_01::BeginPlay(){
+	Super::BeginPlay();	
+}
+
+void AItem_01::Tick(float DeltaTime){
+	Super::Tick(DeltaTime);
+}
+
+ // //////////////////////////////////////////////////////////
+void AItem_01::SetupValues(FString name, int id){
+	UE_LOG(LogTemp, Warning, TEXT("ktx --SetupValues------------ SetupValues"));
+	SetDetails(name, id);
+	
+	UE_LOG(LogTemp, Warning, TEXT("ktx --SetupValues------------ SkillControl"));
+	SkillControl(); // <-- event run
+	UE_LOG(LogTemp, Warning, TEXT("ktx --SetupValues------------ SkillControl_Implementation"));
+	SkillControl_Implementation(); // <-- local c++ run	
+	
+	UE_LOG(LogTemp, Warning, TEXT("ktx --SetupValues------------ SkillSet"));
+	SkillSet(); // <-- event run
+	
+	UE_LOG(LogTemp, Warning, TEXT("ktx --SetupValues------------ SkillMAX"));
+	bool tempA = SkillMAX(); // 
+	UE_LOG(LogTemp, Warning, TEXT("ktx --SetupValues------------ SkillMAX_Implementation"));
+	bool tempB = SkillMAX_Implementation(); // 
+}
+
+void AItem_01::Check_01() {
+	// dispatche delegate event - can get bind in BP
+	UE_LOG(LogTemp, Warning, TEXT("ktx --Check_01------------ OnImpact.Broadcast()"));
+	OnImpact.Broadcast();
+
+	OnReciveHealth.Broadcast(12.45);
+}
+
+
+bool AItem_01::isActive() {
+	return false;
+}
+
+bool AItem_01::isExtra() {
+	return false;
+}
+
+
+void AItem_01::SkillControl_Implementation() {
+	UE_LOG(LogTemp, Warning, TEXT("ktx ++++++ inside SkillControl_Implementation")); // <-- not running
+}
+
+bool AItem_01::SkillMAX_Implementation() {
+	UE_LOG(LogTemp, Warning, TEXT("ktx ++++++ inside SkillMAX_Implementation"));
+	return false;
+}
